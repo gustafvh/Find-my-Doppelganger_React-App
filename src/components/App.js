e0939e8dafaee2e6d40aba9061e703a8651a7a97
Gustaf Halvardsson <gustaf.halvardsson@gmail.com>
2019-08-11 17:17:24 +0200

    added firebase upload functionality

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import UploadImage from './UploadImage/UploadImage';
+import "./App.scss";
+
+function App() {
+  return (
+    <div>
+    
+    
+    
+
+    <div className="header__container">
+    <h3>Find my </h3> 
+    <h1>Doppelganger </h1> 
+    <UploadImage/>
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UploadImage/UploadImage.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import firebase from "../../config/firebase";
+import "./UploadImage.scss"
+
+export default class UploadImage extends Component {
+
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          uploadedFileUrl: ""
+        };
+
+    }
+
+    componentDidMount() {
+
+      
+    }
+
+    uploadToFirebaseStorage = async (event) => {
+      let fileToUpload = event.target.files[0] 
+      let refNameInFirebase = firebase.storage().ref('UploadedImages/ppicture.jpg')
+      
+      const snapshot = await refNameInFirebase.put(fileToUpload);
+      const imageUrl = await snapshot.ref.getDownloadURL();
+
+      this.setState({
+        uploadedFileUrl: imageUrl
+    })
+
+    }
+
+    
+
+    
+
+    render() {
+        return (
+            <div>
+            <div className="upload-file__container">
+            <input type="file" accept="image/*" onChange={ (event) => this.uploadToFirebaseStorage(event) }/>
+            
+
+            <img src={this.state.uploadedFileUrl} alt=""/>
+            </div>
+            </div>
+        )
+    }
+}
